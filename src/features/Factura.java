d8286df2fce558822674d644fef717b4ee1666cd
SSZA <angel.sanzam@educa.jcyl.es>
2025-12-11 19:26:57 +0100

    Añadida entidad factura con atributos

---
+++ src/features/Factura.java
@@ -0,0 +1,40 @@
+package features;
+
+public class Factura {
+    private int numFactura;
+    private String concepto;
+    private String precio;
+    private String fecha;
+
+    public int getNumFactura() {
+        return numFactura;
+    }
+
+    public void setNumFactura(int numFactura) {
+        this.numFactura = numFactura;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
